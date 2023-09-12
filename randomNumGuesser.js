f9b15e662624a53aee00be5c5e6550e4e92a24a5
Christine Cion <cion.c.bscs@gmail.com>
2023-09-12 20:29:54 +0800

    added random number guessing game

---
+++ randomNumGuesser.js
@@ -0,0 +1,34 @@
+// npm install prompt-sync
+// prompt function - to take user input
+// prompt 'require' to import prompt module
+
+const prompt = require("prompt-sync")();
+
+const target = Math.round(Math.random()*100);
+
+console.log("\t==========================================");
+console.log("\t=                                        =");
+console.log("\t=    WELCOME TO NUMBER GUESSING GAME     =");
+console.log("\t=                                        =");
+console.log("\t==========================================");
+
+let guesses = 0;
+// console.log(target);
+
+while(true){
+    guesses++;
+
+    const guess = Number(prompt("Guess the number (0-100): "));  
+    if (guess > target) {
+    console.log("Your guess is too high. Try again.");
+    }
+    else if (guess < target){
+        console.log("Your guess is too low. Try again.");
+    }
+    else{
+        console.log("You got it right!");
+        break;
+    }
+
+}
+console.log("It took you", guesses, "guess/guesses!");
